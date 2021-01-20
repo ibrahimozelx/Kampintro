2666029f0b91c484a4131c9a8ecf5f69b7e686dc
İbrahim Özel <ozelibrahim525@gmail.com>
2021-01-20 23:50:31 +0300

    20 Ocak Ders Sonu

---
+++ Kampintro/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // type safety - tip güvenliği
+            //Do not repeat yourself- Kendini tekrarlama
+            // değer tutucu
+            
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarBugun = 7.55;
+            double dolarDun = 8.05;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+        }
+    }
+}
---
+++ Kampsart/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kampsart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            for (int i = 1; i <= 10; i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
